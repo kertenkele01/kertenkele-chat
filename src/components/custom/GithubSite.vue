004cbb03cf096051853af940c10e4a951ac16dc7
kertenkele01 <kadircirak43@gmail.com>
2023-03-10 06:45:34 +0300

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,6 +1,6 @@
-<template>
+ke<template>
   <div class="text-neutral-400">
-    <span>Star on</span>
+    <span>kertenkele Chat Bot</span>
     <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
       GitHub
     </a>
